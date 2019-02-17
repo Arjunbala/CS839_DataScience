0617bdc11fb8a0b462030abe507b468d295c1ed1
Rohit Kumar Sharma <rsharma@stark.cs.wisc.edu>
2019-02-17 00:12:40 -0600

    Added code to create candidate tokens of length 1, 2, 3 and 4

--- Part1-NER/src/preprocess.py
+++ Part1-NER/src/preprocess.py
@@ -1,6 +1,33 @@
 import nltk
 
 
+def tokenize_candidates(doc):
+	"""
+	Tokenizes the string from a document into candidate tokens.
+	Combines consecutive tokens of size 1, 2, 3 and 4 to create candidate tokens.
+	:param doc: string, the text to be tokenized
+	:return: list, the list of candidate tokens of length 1, 2, 3 and 4
+	"""
+	tokens = nltk.word_tokenize(doc)
+	print(tokens)
+
+	candidates = []
+	for idx, token in zip(range(len(tokens)), tokens):
+		# Add length one tokens
+		candidates.append(token)
+		# Add length two tokens
+		if idx - 1 >= 0:
+			candidates.append(tokens[idx - 1] + ' ' + token)
+		# Add length two tokens
+		if idx - 2 >= 0:
+			candidates.append(tokens[idx - 2] + ' ' + tokens[idx - 1] + ' ' + tokens[idx])
+		# Add length two tokens
+		if idx - 3 >= 0:
+			candidates.append(tokens[idx - 3] + ' ' + tokens[idx - 2] + ' ' + tokens[idx - 1] + ' ' + tokens[idx])
+
+	return candidates
+
+
 def main():
 	pass
 
