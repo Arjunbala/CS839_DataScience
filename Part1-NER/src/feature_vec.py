e16bdc79fa7258b7b5c18bac73e999cf33471107
Arjun Balasubramanian <arjunbala0711@gmail.com>
2019-02-22 22:48:42 -0600

    Commit API interfaces for feature extraction from a candidate

--- Part1-NER/src/feature_vec.py
+++ Part1-NER/src/feature_vec.py
@@ -1,10 +1,29 @@
 def main():
-	pass
+    pass
 
 
-def get_feature_vec():
-	pass
+def get_feature_vec(candidate):
+    pass
 
+def is_capitalized(candidate):
+    pass
 
+def is_dictionary_word(candidate):
+    pass
+
+def get_number_of_token(candidate):
+    pass
+
+def get_length_of_individual_tokens(candidates):
+    pass
+
+def get_prefix_class(candidate):
+    pass
+
+def get_suffix_class(candidate):
+    pass
+
+def get_part_of_speech(candidate):
+    pass
 if __name__ == "__main__":
-	main()
+    main()
