a4f0b6b52da19624321fc9e9d1d398006bbe94e0
aditaker <adityarungta2008@gmail.com>
2019-02-22 23:33:26 -0600

    Feature generation done

--- Part1-NER/src/feature_vec.py
+++ Part1-NER/src/feature_vec.py
@@ -1,29 +1,54 @@
+import numpy as np
+
+#Everything else is garbage prefix
+possible_location_prefixes = ['at','in','of']
+
+#Everything else is garbage suffix
+possible_location_suffixes = []
+
+
 def main():
-    pass
+    get_feature_vec(['','Hello India Walo',''])
 
 
 def get_feature_vec(candidate):
-    pass
+    features = [];
+    features.append(is_capitalized(candidate));
+    features.extend(get_number_of_tokens(candidate));
+    features.append(get_prefix_class(candidate));
+    features.append(get_suffix_class(candidate));
+    print(features);
+    return np.asarray(features)
 
 def is_capitalized(candidate):
-    pass
+    for i in range(len(candidate[1])):
+        if not candidate[1][i][0].isupper() :
+            return 0
+    return 1
 
 def is_dictionary_word(candidate):
     pass
 
-def get_number_of_token(candidate):
-    pass
+def get_number_of_tokens(candidate):
+    length_of_candidate = len(candidate[1].split(' '))
+    return list(np.eye(4)[length_of_candidate-1]);
 
 def get_length_of_individual_tokens(candidates):
     pass
 
 def get_prefix_class(candidate):
-    pass
+    if candidate[0] in possible_location_prefixes :
+        return 1;
+    return 0;
 
 def get_suffix_class(candidate):
-    pass
+    if candidate[2] in possible_location_suffixes :
+        return 1;
+    return 0;
 
 def get_part_of_speech(candidate):
     pass
+
 if __name__ == "__main__":
     main()
+          
