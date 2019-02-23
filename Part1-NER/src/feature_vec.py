eeb9c8b1673684659aafe51bbee07870dba794a6
Rohit Sharma <rohit.sharma@wisc.edu>
2019-02-23 11:32:44 -0600

    Added some comments

--- Part1-NER/src/feature_vec.py
+++ Part1-NER/src/feature_vec.py
@@ -1,54 +1,66 @@
 import numpy as np
 
-#Everything else is garbage prefix
-possible_location_prefixes = ['at','in','of']
+# Everything else is garbage prefix
+possible_location_prefixes = ['at', 'in', 'of']
 
-#Everything else is garbage suffix
+# Everything else is garbage suffix
 possible_location_suffixes = []
 
 
-def main():
-    get_feature_vec(['','Hello India Walo',''])
-
-
 def get_feature_vec(candidate):
-    features = [];
-    features.append(is_capitalized(candidate));
-    features.extend(get_number_of_tokens(candidate));
-    features.append(get_prefix_class(candidate));
-    features.append(get_suffix_class(candidate));
-    print(features);
+    """
+    
+    :param candidate:
+    :return:
+    """
+    features = []
+    features.append(is_capitalized(candidate))
+    features.extend(get_number_of_tokens(candidate))
+    features.append(get_prefix_class(candidate))
+    features.append(get_suffix_class(candidate))
+    print(features)
     return np.asarray(features)
+
 
 def is_capitalized(candidate):
     for i in range(len(candidate[1])):
-        if not candidate[1][i][0].isupper() :
+        if not candidate[1][i][0].isupper():
             return 0
     return 1
+
 
 def is_dictionary_word(candidate):
     pass
 
+
 def get_number_of_tokens(candidate):
     length_of_candidate = len(candidate[1].split(' '))
-    return list(np.eye(4)[length_of_candidate-1]);
+    return list(np.eye(4)[length_of_candidate - 1])
+
 
 def get_length_of_individual_tokens(candidates):
     pass
 
+
 def get_prefix_class(candidate):
-    if candidate[0] in possible_location_prefixes :
-        return 1;
-    return 0;
+    if candidate[0] in possible_location_prefixes:
+        return 1
+    return 0
+
 
 def get_suffix_class(candidate):
-    if candidate[2] in possible_location_suffixes :
-        return 1;
-    return 0;
+    if candidate[2] in possible_location_suffixes:
+        return 1
+    return 0
+
 
 def get_part_of_speech(candidate):
     pass
 
+
+def main():
+    get_feature_vec(['', 'Hello India Walo', ''])
+
+
 if __name__ == "__main__":
     main()
-          
--- Part1-NER/src/preprocess.py
+++ Part1-NER/src/preprocess.py
@@ -5,82 +5,15 @@ import pandas as pd
 from feature_vec import get_feature_vec
 
 
-def tokenize_candidates(doc):
-    """
-    Tokenizes the string from a document into candidate tokens.
-    Combines consecutive tokens of size 1, 2, 3 and 4 to create candidate tokens.
-    :param doc: string, the text to be tokenized
-    :return: list, the list of candidate tokens of length 1, 2, 3 and 4
-    """
-    tokens = nltk.word_tokenize(doc)
-    # print(tokens)
-
-    candidates = []
-    for idx, token in zip(range(len(tokens)), tokens):
-        # Add length one tokens
-        candidates.append([get_prefix_token(tokens,idx), token, get_suffix_token(tokens,idx)])
-        # Add length two tokens
-        if idx - 1 >= 0:
-            candidates.append([get_prefix_token(tokens,idx-1), tokens[idx - 1] + ' ' + token, get_suffix_token(tokens,idx)])
-        # Add length two tokens
-        if idx - 2 >= 0:
-            candidates.append([get_prefix_token(tokens,idx-2), tokens[idx - 2] + ' ' + tokens[idx - 1] + ' ' + tokens[idx], get_suffix_token(tokens,idx)])
-        # Add length two tokens
-        if idx - 3 >= 0:
-            candidates.append([get_prefix_token(tokens,idx-3), tokens[idx - 3] + ' ' + tokens[idx - 2] + ' ' + tokens[idx - 1] + ' ' + tokens[idx], get_suffix_token(tokens,idx)])
-
-    return candidates
-
-
-def get_prefix_token(tokens, index):
-    if index == 0:
-        return ''
-    return tokens[index-1]
-
-
-def get_suffix_token(tokens, index):
-    if index == len(tokens)-1:
-        return ''
-    return tokens[index+1]
-
-
-def convert_integer_file_number_to_string(file_number):
-    return '{0:03d}'.format(file_number)
-
-
-def get_document_string(file_type, file_number):
-    if file_type == 'annotated':
-        filename = '../dataset/' + convert_integer_file_number_to_string(file_number) + '.txt'
-    elif file_type == 'raw':
-        filename = '../raw-dataset/' + convert_integer_file_number_to_string(file_number) + '.txt'
-    else:
-        return ''
-
-    with open(filename, 'r') as f:
-        document_string = f.read()
-    return document_string
-
-
-def fetch_annotated_tokens(file_number):
-    # Read all lines from the given file
-    document_string = get_document_string('annotated', file_number)
-
-    # Now, read line by line
-    annotated_tokens = []
-    # Find the positions of <loc> and </loc>
-    start_positions = list(re.finditer('<loc>', document_string))
-    end_positions = list(re.finditer('</loc>', document_string))
-    for i in range(0, len(start_positions)):
-        # Extract out the part between <loc> and </loc>
-        annotated_tokens.append(document_string[start_positions[i].end():end_positions[i].start()])
-    return annotated_tokens
-
-
 def label_dataset():
+    """
+    For each word, labels it as 1 if it is a location, else 0. Also transforms the word to its feature vector
+    :return: DataFrame, 1st column: feature vector of the word, 2nd column: label indicating location or not
+    """
     X = []
     y = []
 
-    for filenum in range(1, 300):
+    for filenum in range(1, 301):
         # print(filenum)
         doc_str = get_document_string('raw', filenum)
         candidates = tokenize_candidates(doc_str)
@@ -94,6 +27,110 @@ def label_dataset():
                 y.append(0)
 
     return pd.DataFrame({'X': X, 'y': y})
+
+
+def tokenize_candidates(doc):
+    """
+    Tokenizes the string from a document into candidate tokens.
+    Combines consecutive tokens of size 1, 2, 3 and 4 to create candidate tokens.
+    :param doc: string, the text to be tokenized
+    :return: list, the list of candidate tokens of length 1, 2, 3 and 4
+    """
+    tokens = nltk.word_tokenize(doc)
+    # print(tokens)
+
+    candidates = []
+    for idx, token in zip(range(len(tokens)), tokens):
+        # Add length one tokens
+        candidates.append([get_prefix_token(tokens, idx), token, get_suffix_token(tokens, idx)])
+        # Add length two tokens
+        if idx - 1 >= 0:
+            candidates.append(
+                [get_prefix_token(tokens, idx - 1), tokens[idx - 1] + ' ' + token, get_suffix_token(tokens, idx)])
+        # Add length two tokens
+        if idx - 2 >= 0:
+            candidates.append(
+                [get_prefix_token(tokens, idx - 2), tokens[idx - 2] + ' ' + tokens[idx - 1] + ' ' + tokens[idx],
+                 get_suffix_token(tokens, idx)])
+        # Add length two tokens
+        if idx - 3 >= 0:
+            candidates.append([get_prefix_token(tokens, idx - 3),
+                               tokens[idx - 3] + ' ' + tokens[idx - 2] + ' ' + tokens[idx - 1] + ' ' + tokens[idx],
+                               get_suffix_token(tokens, idx)])
+
+    return candidates
+
+
+def get_prefix_token(tokens, index):
+    """
+
+    :param tokens:
+    :param index:
+    :return:
+    """
+    if index == 0:
+        return ''
+    return tokens[index - 1]
+
+
+def get_suffix_token(tokens, index):
+    """
+
+    :param tokens:
+    :param index:
+    :return:
+    """
+    if index == len(tokens) - 1:
+        return ''
+    return tokens[index + 1]
+
+
+def convert_integer_file_number_to_string(file_number):
+    """
+    Transforms an integer into proper file name format, ex: 055.txt
+    :param file_number: int, file number
+    :return: string, 0 appended string of length 3 for the file name
+    """
+    return '{0:03d}'.format(file_number)
+
+
+def get_document_string(file_type, file_number):
+    """
+    Reads the required annotated/raw file with file_number as its name into a string.
+    :param file_type: string, 'annotated' if annotated file is desired. else 'raw'
+    :param file_number: int, file number
+    :return: string, the contents of the file
+    """
+    if file_type == 'annotated':
+        filename = '../dataset/' + convert_integer_file_number_to_string(file_number) + '.txt'
+    elif file_type == 'raw':
+        filename = '../raw-dataset/' + convert_integer_file_number_to_string(file_number) + '.txt'
+    else:
+        return ''
+
+    with open(filename, 'r') as f:
+        document_string = f.read()
+    return document_string
+
+
+def fetch_annotated_tokens(file_number):
+    """
+    Retrieves the location tokens based on the <loc></loc> tags from an annotated file
+    :param file_number: int, file number
+    :return: list, all the location tokens from the annotated file
+    """
+    # Read all lines from the given file
+    document_string = get_document_string('annotated', file_number)
+
+    # Now, read line by line
+    annotated_tokens = []
+    # Find the positions of <loc> and </loc>
+    start_positions = list(re.finditer('<loc>', document_string))
+    end_positions = list(re.finditer('</loc>', document_string))
+    for i in range(0, len(start_positions)):
+        # Extract out the part between <loc> and </loc>
+        annotated_tokens.append(document_string[start_positions[i].end():end_positions[i].start()])
+    return annotated_tokens
 
 
 def main():
