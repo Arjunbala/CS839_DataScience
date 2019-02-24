c58497441bdff1fc9a0069b8a41e7cd2ec5d6249
Rohit Sharma <rohit.sharma@wisc.edu>
2019-02-23 21:36:02 -0600

    Added comments

--- Part1-NER/src/model.py
+++ Part1-NER/src/model.py
@@ -6,12 +6,28 @@ from sklearn.linear_model import LogisticRegression
 
 
 def train_logistic_regression(X_train, y_train, X_test, y_test):
+	"""
+	Trains a logistic regression model on the training data and returns the accuracy on the testing data
+	:param X_train: ndarray, feature vectors for candidates for training
+	:param y_train: list, class labels for training
+	:param X_test: ndarray, feature vectors for candidates for testing
+	:param y_test: list, class labels for testing
+	:return: float, accuracy of the logistic regression model
+	"""
 	logistic_clf = LogisticRegression()
 	logistic_clf.fit(X_train, y_train)
 	return logistic_clf.score(X_test, y_test)
 
 
 def train_svm(X_train, y_train, X_test, y_test):
+	"""
+	Trains n SVM model on the training data and returns the accuracy on the testing data
+	:param X_train: ndarray, feature vectors for candidates for training
+	:param y_train: list, class labels for training
+	:param X_test: ndarray, feature vectors for candidates for testing
+	:param y_test: list, class labels for training
+	:return: float, accuracy of the SVM model
+	"""
 	svm_clf = SVC()
 	svm_clf.fit(X_train, y_train)
 	return svm_clf.score(X_test, y_test)
