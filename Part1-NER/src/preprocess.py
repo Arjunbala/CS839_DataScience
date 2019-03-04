897c98ba4df834763c4c0362373a679c047da571
Rohit Sharma <rohit.sharma@wisc.edu>
2019-03-03 21:05:45 -0600

    Increasing number of negative samples

--- Part1-NER/src/preprocess.py
+++ Part1-NER/src/preprocess.py
@@ -42,8 +42,8 @@ def label_dataset(file_list, isTraining):
                 X_neg.append(get_feature_vec(candidate))
                 y_neg.append(0)
     if isTraining:
-        X_neg = X_neg[0:2*len(X_pos)]
-        y_neg = y_neg[0:2*len(X_pos)]
+        X_neg = X_neg[0:20*len(X_pos)]
+        y_neg = y_neg[0:20*len(X_pos)]
     X = []
     X.extend(X_neg)
     X.extend(X_pos)
