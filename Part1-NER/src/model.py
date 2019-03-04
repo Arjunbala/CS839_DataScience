7a323c6f0bc49eaf9c1811bd2e632db0c63e2115
Rohit Sharma <rohit.sharma@wisc.edu>
2019-03-04 12:57:22 -0600

    Added code to evaluate on test set

--- Part1-NER/src/model.py
+++ Part1-NER/src/model.py
@@ -12,6 +12,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_recall_fscore_support
 
 
+# TODO: Write a common method which predicts taking into consideration white list along with predictions returned by model and use it to predict everywhere
 white_list = ['New York', 'Los Angeles']
 
 
@@ -89,28 +90,120 @@ def find_false_positives(actual_candidates, y_test, predictions):
 			print(actual_candidates[i] + " pred->" + str(predictions[i]) +" actual->" + str(y_test[i]))
 
 
+def cross_validation(dev_set, n_folds):
+	kf = KFold(n_splits=n_folds)
+	kf.get_n_splits(dev_set)
+	print('## Running Cross Validation: ##')
+	dt_prec = dt_rec = 0
+	rf_prec = rf_rec = 0
+	lr_prec = lr_rec = 0
+	svm_prec = svm_rec = 0
+	for train_indices, validation_indices in kf.split(dev_set):
+		train_points = []
+		validation_points = []
+		for idx in train_indices:
+			train_points.append(dev_set[idx])
+		for idx in validation_indices:
+			validation_points.append(dev_set[idx])
+
+		train_set = label_dataset(train_points, True)
+		np.savetxt("train.csv", np.column_stack((train_set[0], train_set[1])), delimiter=",")
+		validation_set = label_dataset(validation_points, False)
+
+		prec, rec, f1, supp = train_decision_tree(train_set[0], train_set[1], validation_set[0], validation_set[1])
+		dt_prec += prec
+		dt_rec += rec
+
+		prec, rec, f1, supp = train_random_forest(train_set[0], train_set[1], validation_set[0], validation_set[1])
+		rf_prec += prec
+		rf_rec += rec
+
+		prec, rec, f1, supp = train_logistic_regression(train_set[0], train_set[1], validation_set[0], validation_set[1])
+		lr_prec += prec
+		lr_rec += rec
+
+		prec, rec, f1, supp = train_svm(train_set[0], train_set[1], validation_set[2], validation_set[0], validation_set[1])
+		svm_prec += prec
+		svm_rec += rec
+
+	dt_prec /= n_folds
+	dt_rec /= n_folds
+	dt_f1 = 2 * dt_prec * dt_rec / (dt_prec + dt_rec)
+
+	best_f1 = dt_f1
+	best_model = 'Decision Tree'
+
+	rf_prec /= n_folds
+	rf_rec /= n_folds
+	rf_f1 = 2 * rf_prec * rf_rec / (rf_prec + rf_rec)
+	if rf_f1 > best_f1:
+		best_f1 = rf_f1
+		best_model = 'Random Forest'
+
+	lr_prec /= n_folds
+	lr_rec /= n_folds
+	lr_f1 = 2 * lr_prec * lr_rec / (lr_prec + lr_rec)
+	if lr_f1 > best_f1:
+		best_f1 = lr_f1
+		best_model = 'Logistic Regression'
+	
+	svm_prec /= n_folds
+	svm_rec /= n_folds
+	svm_f1 = 2 * svm_prec * svm_rec / (svm_prec + svm_rec)
+	if svm_f1 > best_f1:
+		best_f1 = svm_f1
+		best_model = 'SVM'
+	
+	print('Decision Tree:', dt_prec, dt_rec)
+	print('Random Forest:', rf_prec, rf_rec)
+	print('Logistic Regression:', lr_prec, lr_rec)
+	print('SVM:', svm_prec, svm_rec)
+	print()
+	print('Best model:', best_model)
+	print('F1 score:', best_f1)
+	print()
+	return best_model
+
+
+def train_model(algorithm, X_train, y_train):
+	if algorithm == 'SVM':
+		svm_clf = SVC(gamma='scale')
+		svm_clf.fit(X_train, y_train)
+		return svm_clf
+	else:
+		print('Unknown algorithm to build a model')
+		return None
+
+
+def evaluate_model(model, X_test_vec, X_test_str, y_test):
+	y_pred = model.predict(X_test_vec)
+
+	# White list the predictions
+	for idx, x_str in zip(range(len(X_test_str)), X_test_str):
+		if x_str in white_list:
+			y_pred[idx] = 1
+	
+	precision, recall, f1, _ = precision_recall_fscore_support(y_test, y_pred, average='binary')
+	print('Model performance on test set:')
+	print('Precision:\t', precision)
+	print('Recall:\t\t', recall)
+	print('F1:\t\t', f1)
+	return precision, recall, f1
+	
+
 def main():
 	dev_set, test_set = get_file_numbers()
-	kf = KFold(n_splits=4)
-	kf.get_n_splits(dev_set)
-	for train_indices, validation_indices in kf.split(dev_set):
-                print("## Model ####")
-                train_points = []
-                validation_points = []
-                for idx in train_indices:
-                    train_points.append(dev_set[idx])
-                for idx in validation_indices:
-                    validation_points.append(dev_set[idx])
 
-                train_set = label_dataset(train_points, True)
-                np.savetxt("train.csv", np.column_stack((train_set[0], train_set[1])), delimiter=",")
-                validation_set = label_dataset(validation_points, False)
+	best_model = cross_validation(dev_set, 4)
+	
+	dev_dataset = label_dataset(dev_set, True)
+	model = train_model(best_model, dev_dataset[0], dev_dataset[1])
 
-                print('Decision Tree:', train_decision_tree(train_set[0], train_set[1], validation_set[0], validation_set[1]))
-                print('Random Forest:', train_random_forest(train_set[0], train_set[1], validation_set[0], validation_set[1]))
-                print('Logistic Regression:', train_logistic_regression(train_set[0], train_set[1], validation_set[0], validation_set[1]))
-                print('SVM:', train_svm(train_set[0], train_set[1], validation_set[2], validation_set[0], validation_set[1]))
-                print()
+	# Evaluate on test set
+	test_dataset = label_dataset(test_set, True)
+	print('## Evaluating on Test Set: ##')
+	model_prec, model_rec, model_f1 = evaluate_model(model, test_dataset[0], test_dataset[2], test_dataset[1])
+
 
 if __name__ == "__main__":
 	main()
