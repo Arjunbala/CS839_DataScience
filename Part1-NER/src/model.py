9c692cb0df2b38286d9bcbf7cdb69c3dcfc9e070
Rohit Sharma <rohit.sharma@wisc.edu>
2019-03-04 22:04:07 -0600

    Created a predict method which takes care of black listing and white listing

--- Part1-NER/src/model.py
+++ Part1-NER/src/model.py
@@ -12,82 +12,94 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_recall_fscore_support
 
 
-# TODO: Write a common method which predicts taking into consideration white list along with predictions returned by model and use it to predict everywhere
 white_list = ['New York', 'Los Angeles']
 
 
-def train_decision_tree(X_train, y_train, X_test, y_test):
+def predict(classifier, X_vecs, X_strs):
+	"""
+	Uses the trained classifier to predict labels on X_vecs data. Then, handles white listing and black listing
+	for those predictions.
+	:param classifier: model
+	:param X_vecs: ndarray, dataset for which predictions are to be made
+	:param X_strs: list, list of actual tokens corresponding to the feature vectors in X_vecs
+	:return: list, predictions
+	"""
+	predictions = classifier.predict(X_vecs)
+
+	# White list the predictions
+	for idx, x_str in zip(range(len(X_strs)), X_strs):
+		if x_str in white_list:
+			predictions[idx] = 1
+
+	return predictions
+
+
+def train_decision_tree(X_train, y_train, X_test_vecs, X_test_strs, y_test):
 	"""
 	Trains a decision tree model on the training data and returns the accuracy on the testing data
 	:param X_train: ndarray, feature vectors for candidates for training
 	:param y_train: list, class labels for training
-	:param X_test: ndarray, feature vectors for candidates for testing
+	:param X_test_vecs: ndarray, feature vectors for candidates for testing
 	:param y_test: list, class labels for testing
 	:return: (float, float, float, int), precision, recall, fbeta_score, support
 	"""
 	dt_clf = tree.DecisionTreeClassifier()
 	dt_clf.fit(X_train, y_train)
-	predictions = dt_clf.predict(X_test)
-	return precision_recall_fscore_support(y_test, predictions, average='binary')		# dt_clf.score(X_test, y_test),
+	predictions = predict(dt_clf, X_test_vecs, X_test_strs)
+	return precision_recall_fscore_support(y_test, predictions, average='binary')
 
 
-def train_random_forest(X_train, y_train, X_test, y_test):
+def train_random_forest(X_train, y_train, X_test_vecs, X_test_strs, y_test):
 	"""
 	Trains a random forest model on the training data and returns the accuracy on the testing data
 	:param X_train: ndarray, feature vectors for candidates for training
 	:param y_train: list, class labels for training
-	:param X_test: ndarray, feature vectors for candidates for testing
+	:param X_test_vecs: ndarray, feature vectors for candidates for testing
 	:param y_test: list, class labels for testing
 	:return: (float, float, float, int), precision, recall, fbeta_score, support
 	"""
 	randomforest_clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
 	randomforest_clf.fit(X_train, y_train)
-	predictions = randomforest_clf.predict(X_test)
-	return precision_recall_fscore_support(y_test, predictions, average='binary')  # randomforest_clf.score(X_test, y_test)
+	predictions = predict(randomforest_clf, X_test_vecs, X_test_strs)
+	return precision_recall_fscore_support(y_test, predictions, average='binary')
 
 
-def train_logistic_regression(X_train, y_train, X_test, y_test):
+def train_logistic_regression(X_train, y_train, X_test_vecs, X_test_strs, y_test):
 	"""
 	Trains a logistic regression model on the training data and returns the accuracy on the testing data
 	:param X_train: ndarray, feature vectors for candidates for training
 	:param y_train: list, class labels for training
-	:param X_test: ndarray, feature vectors for candidates for testing
+	:param X_test_vecs: ndarray, feature vectors for candidates for testing
 	:param y_test: list, class labels for testing
 	:return: (float, float, float, int), precision, recall, fbeta_score, support
 	"""
 	logistic_clf = LogisticRegression(solver='liblinear')
 	logistic_clf.fit(X_train, y_train)
-	predictions = logistic_clf.predict(X_test)
-	return precision_recall_fscore_support(y_test, predictions, average='binary')  # logistic_clf.score(X_test, y_test)
+	predictions = predict(logistic_clf, X_test_vecs, X_test_strs)
+	return precision_recall_fscore_support(y_test, predictions, average='binary')
 
 
-def train_svm(X_train, y_train, actual_candidates, X_test, y_test):
+def train_svm(X_train, y_train, X_test_vecs, X_test_strs, y_test):
 	"""
 	Trains n SVM model on the training data and returns the accuracy on the testing data
 	:param X_train: ndarray, feature vectors for candidates for training
 	:param y_train: list, class labels for training
-	:param X_test: ndarray, feature vectors for candidates for testing
+	:param X_test_vecs: ndarray, feature vectors for candidates for testing
 	:param y_test: list, class labels for training
 	:return: (float, float, float, int), precision, recall, fbeta_score, support
 	"""
-	class_wts = 'balanced'	# {0: 1, 1: 10000}
 	svm_clf = SVC(gamma='scale')
 	svm_clf.fit(X_train, y_train)
-	predictions = svm_clf.predict(X_test)
+	predictions = predict(svm_clf, X_test_vecs, X_test_strs)
 
-	# White list the predictions
-	for idx, candidate in zip(range(len(actual_candidates)), actual_candidates):
-		if candidate in white_list:
-			predictions[idx] = 1
-	
-	#find_false_positives(actual_candidates, y_test, predictions)
-	return precision_recall_fscore_support(y_test, predictions, average='binary')  # svm_clf.score(X_test, y_test)
+	# find_false_positives(X_test_strs, y_test, predictions)
+	return precision_recall_fscore_support(y_test, predictions, average='binary')
 
 
-def find_false_positives(actual_candidates, y_test, predictions):
+def find_false_positives(X_test_strs, y_test, predictions):
 	for i in range(0, len(predictions)):
 		if predictions[i] != y_test[i]:
-			print(actual_candidates[i] + " pred->" + str(predictions[i]) +" actual->" + str(y_test[i]))
+			print(X_test_strs[i] + " pred->" + str(predictions[i]) + " actual->" + str(y_test[i]))
 
 
 def cross_validation(dev_set, n_folds):
@@ -107,22 +119,21 @@ def cross_validation(dev_set, n_folds):
 			validation_points.append(dev_set[idx])
 
 		train_set = label_dataset(train_points, True)
-		np.savetxt("train.csv", np.column_stack((train_set[0], train_set[1])), delimiter=",")
 		validation_set = label_dataset(validation_points, False)
 
-		prec, rec, f1, supp = train_decision_tree(train_set[0], train_set[1], validation_set[0], validation_set[1])
+		prec, rec, f1, supp = train_decision_tree(train_set[0], train_set[1], validation_set[0], validation_set[2], validation_set[1])
 		dt_prec += prec
 		dt_rec += rec
 
-		prec, rec, f1, supp = train_random_forest(train_set[0], train_set[1], validation_set[0], validation_set[1])
+		prec, rec, f1, supp = train_random_forest(train_set[0], train_set[1], validation_set[0], validation_set[2], validation_set[1])
 		rf_prec += prec
 		rf_rec += rec
 
-		prec, rec, f1, supp = train_logistic_regression(train_set[0], train_set[1], validation_set[0], validation_set[1])
+		prec, rec, f1, supp = train_logistic_regression(train_set[0], train_set[1], validation_set[0], validation_set[2], validation_set[1])
 		lr_prec += prec
 		lr_rec += rec
 
-		prec, rec, f1, supp = train_svm(train_set[0], train_set[1], validation_set[2], validation_set[0], validation_set[1])
+		prec, rec, f1, supp = train_svm(train_set[0], train_set[1], validation_set[0], validation_set[2], validation_set[1])
 		svm_prec += prec
 		svm_rec += rec
 
@@ -175,14 +186,8 @@ def train_model(algorithm, X_train, y_train):
 		return None
 
 
-def evaluate_model(model, X_test_vec, X_test_str, y_test):
-	y_pred = model.predict(X_test_vec)
-
-	# White list the predictions
-	for idx, x_str in zip(range(len(X_test_str)), X_test_str):
-		if x_str in white_list:
-			y_pred[idx] = 1
-	
+def evaluate_model(model, X_test_vecs, X_test_strs, y_test):
+	y_pred = predict(model, X_test_vecs, X_test_strs)
 	precision, recall, f1, _ = precision_recall_fscore_support(y_test, y_pred, average='binary')
 	print('Model performance on test set:')
 	print('Precision:\t', precision)
