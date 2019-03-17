b1b9402b7aaf372d13f595588b1fded1b7d918a6
Rohit Sharma <rohit.sharma@wisc.edu>
2019-03-16 23:17:32 -0500

    Changed IMDB URL to 10000 movies list

--- Part2-WebCrawling/src/extract_imdb.py
+++ Part2-WebCrawling/src/extract_imdb.py
@@ -2,11 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 
 # TODO: Update URL
-url = 'https://www.imdb.com/search/title?genres=action&start=0&explore=title_type,genres&ref_=adv_nxt'
+url = 'https://www.imdb.com/list/ls063676189/'
 resp = requests.get(url)
 soup = BeautifulSoup(resp.text, 'html.parser')
-movie_list = soup.find_all('h3',{'class':'lister-item-header'})
+movie_list = soup.find_all('h3', {'class': 'lister-item-header'})
 for x in movie_list:
     for y in x.find_all('a'):
         print(y.text)
-
