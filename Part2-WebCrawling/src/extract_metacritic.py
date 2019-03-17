d8bfdc9700fecf5ca50122b1b092c549c6d606e4
Rohit Sharma <rohit.sharma@wisc.edu>
2019-03-16 22:46:35 -0500

    Added draft code to extract movie names from Metacritic.com

---
+++ Part2-WebCrawling/src/extract_metacritic.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+# TODO: Update URL
+url = 'https://www.metacritic.com/browse/movies/score/metascore/all/filtered'
+session = requests.Session()
+
+resp = session.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+soup = BeautifulSoup(resp.text, 'html.parser')
+movie_list = soup.find_all('a', {'class': 'title'})
+for x in movie_list:
+    for y in x.find_all('h3'):
+        print(y.text)
