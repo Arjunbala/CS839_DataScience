a8555e1675d1527f6a2a364fe06869d7e811e475
Rohit Sharma <rohit.sharma@wisc.edu>
2019-03-17 22:11:32 -0500

    Added a draft for stage 2 report. Also reorganized the code a bit.

--- Part2-WebCrawling/src/extract_imdb.py
+++ Part2-WebCrawling/src/extract_imdb.py
@@ -1,6 +1,7 @@
-from bs4 import BeautifulSoup
 import csv
 import requests
+from bs4 import BeautifulSoup
+
 
 def extract_movie_names(soup):
     movie_list = []
@@ -10,12 +11,14 @@ def extract_movie_names(soup):
             movie_list.append(y.text)
     return movie_list
 
+
 def extract_movie_years(soup):
     year_list = []
     movies = soup.find_all('span', {'class': 'lister-item-year'})
     for x in movies:
         year_list.append(x.text.split('(')[-1].split(')')[0])
     return year_list
+
 
 def extract_runtime(soup):
     runtime_list = []
@@ -24,16 +27,18 @@ def extract_runtime(soup):
         runtime_list.append(x.text)
     return runtime_list
 
+
 def extract_user_rating(soup):
     rating_list = []
     movies = soup.find_all('div', {'class': 'ipl-rating-star small'})
     for rating in movies:
-        rating_list.append(rating.find('span',{'class':'ipl-rating-star__rating'}).text)
+        rating_list.append(rating.find('span', {'class': 'ipl-rating-star__rating'}).text)
     return rating_list
+
 
 def extract_metascores(soup):
     metascore_list = []
-    movies = soup.find_all('div',{'class':'lister-item mode-detail'})
+    movies = soup.find_all('div', {'class': 'lister-item mode-detail'})
     for movie in movies:
         #print(movie)
         metascore = movie.find('span', {'class':'metascore'})
@@ -43,72 +48,80 @@ def extract_metascores(soup):
             metascore_list.append('')
     return metascore_list
 
+
 def extract_directors(soup):
     directors_list = []
-    movies = soup.find_all('p',{'class':'text-muted text-small'})
+    movies = soup.find_all('p', {'class': 'text-muted text-small'})
     for movie in movies:
         names = movie.find_all('a')
         if len(names) > 0:
             directors_list.append(names[0].text) 
     return directors_list
 
+
 def extract_actors(soup):
     actors_list = []
-    movies = soup.find_all('p',{'class':'text-muted text-small'})
+    movies = soup.find_all('p', {'class': 'text-muted text-small'})
     for movie in movies:
         names = movie.find_all('a')
         if len(names) > 0:
-            all_names = ""
+            all_names = ''
             for i in range(1, len(names)):
-                all_names = all_names + names[i].text + ", "
+                all_names = all_names + names[i].text + ', '
             actors_list.append(all_names[0:len(all_names)-2])
     return actors_list
 
+
 def extract_genres(soup):
     genre_list = []
-    movies = soup.find_all('span',{'class':'genre'})
+    movies = soup.find_all('span', {'class': 'genre'})
     for movie in movies:
         genre_list.append(movie.text.strip())
     return genre_list
 
 
-base_url = 'https://www.imdb.com/list/ls063676189/?sort=list_order,asc&st_dt=&mode=detail&page='
-movie_dict_list = []
-id_no = 1
-csv_columns = ['ID', 'Name', 'Year', 'Runtime', 'User Rating', 'Metascore', 'Director', 'Actors', 'Genre']
-for page_no in range(1,51):
-    page_url = 'https://www.imdb.com/list/ls063676189/?sort=list_order,asc&st_dt=&mode=detail&page=' + str(page_no)
-    print("Extracting from " + page_url)
-    resp = requests.get(page_url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    names = extract_movie_names(soup)
-    years = extract_movie_years(soup)
-    runtimes = extract_runtime(soup)
-    user_ratings = extract_user_rating(soup)
-    metascores = extract_metascores(soup)
-    directors = extract_directors(soup)
-    actors = extract_actors(soup)
-    genres = extract_genres(soup)
+def main():
+	base_url = 'https://www.imdb.com/list/ls063676189/?sort=list_order,asc&st_dt=&mode=detail&page='
+	movie_dict_list = []
+	id_no = 1
+	csv_columns = ['ID', 'Name', 'Year', 'Runtime', 'User Rating', 'Metascore', 'Director', 'Actors', 'Genre']
+	for page_no in range(1, 51):
+		page_url = 'https://www.imdb.com/list/ls063676189/?sort=list_order,asc&st_dt=&mode=detail&page=' + str(page_no)
+		print('Extracting from ' + page_url)
+		resp = requests.get(page_url)
+		soup = BeautifulSoup(resp.text, 'html.parser')
+		names = extract_movie_names(soup)
+		years = extract_movie_years(soup)
+		runtimes = extract_runtime(soup)
+		user_ratings = extract_user_rating(soup)
+		metascores = extract_metascores(soup)
+		directors = extract_directors(soup)
+		actors = extract_actors(soup)
+		genres = extract_genres(soup)
 
-    for i in range(0, len(names)):
-        movie_dict = {}
-        movie_dict['ID'] = ('%04d' % id_no)
-        id_no = id_no + 1
-        movie_dict['Name'] = names[i]
-        movie_dict['Year'] = years[i]
-        movie_dict['Runtime'] = runtimes[i]
-        movie_dict['User Rating'] = user_ratings[i]
-        movie_dict['Metascore'] = metascores[i]
-        movie_dict['Director'] = directors[i]
-        movie_dict['Actors'] = actors[i]
-        movie_dict['Genre'] = genres[i]
-        movie_dict_list.append(movie_dict)
+		for i in range(0, len(names)):
+			movie_dict = {}
+			movie_dict['ID'] = ('%04d' % id_no)
+			id_no = id_no + 1
+			movie_dict['Name'] = names[i]
+			movie_dict['Year'] = years[i]
+			movie_dict['Runtime'] = runtimes[i]
+			movie_dict['User Rating'] = user_ratings[i]
+			movie_dict['Metascore'] = metascores[i]
+			movie_dict['Director'] = directors[i]
+			movie_dict['Actors'] = actors[i]
+			movie_dict['Genre'] = genres[i]
+			movie_dict_list.append(movie_dict)
 
-try:
-    with open('../data/imdb.csv', 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-        writer.writeheader()
-        for data in movie_dict_list:
-            writer.writerow(data)
-except IOError:
-    print("I/O error")
+	try:
+		with open('../data/imdb.csv', 'w') as csvfile:
+			writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+			writer.writeheader()
+			for data in movie_dict_list:
+				writer.writerow(data)
+	except IOError:
+		print('I/O error')
+
+
+if __name__ == "__main__":
+	main()
--- Part2-WebCrawling/src/extract_metacritic.py
+++ Part2-WebCrawling/src/extract_metacritic.py
@@ -1,14 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-# TODO: Update URL
-url = 'https://www.metacritic.com/browse/movies/score/metascore/all/filtered?page=0'
-session = requests.Session()
 
-resp = session.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+def main():
+	url = 'https://www.metacritic.com/browse/movies/score/metascore/all/filtered?page=0'
+	session = requests.Session()
 
-soup = BeautifulSoup(resp.text, 'html.parser')
-movie_list = soup.find_all('a', {'class': 'title'})
-for x in movie_list:
-    for y in x.find_all('h3'):
-        print(y.text)
+	resp = session.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+	soup = BeautifulSoup(resp.text, 'html.parser')
+	movie_list = soup.find_all('a', {'class': 'title'})
+	for x in movie_list:
+		for y in x.find_all('h3'):
+			print(y.text)
+
+if __name__ == "__main__":
+	main()
